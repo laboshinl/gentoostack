a570ef3d069ecbd9ff81434e2822f05efc1e32dc
Leonid Laboshin <leonid.laboshin@crplab.ru>
2015-03-16 17:08:02 +0300

    fix: Repository 'x-portage' is missing masters attribute

--- chef-omnibus.sh
+++ chef-omnibus.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir -p /usr/local/portage/app-admin/chef-omnibus
+mkdir -p /usr/local/portage/metadata
+echo "masters = gentoo" >> /usr/local/portage/metadata/layout.conf
 echo "PORTDIR_OVERLAY=/usr/local/portage" >> /etc/portage/make.conf
 wget https://raw.githubusercontent.com/laboshinl/gentoo-overlay/master/app-admin/chef-omnibus/chef-omnibus-12.1.1.ebuild -O /usr/local/portage/app-admin/chef-omnibus/chef-omnibus-12.1.1.ebuild
 wget https://raw.githubusercontent.com/laboshinl/gentoo-overlay/master/app-admin/chef-omnibus/Manifest -O /usr/local/portage/app-admin/chef-omnibus/Manifest
