54396024ad769d7c7d7982c84f6b8586ee38d7b7
Leonid Laboshin <leonid.laboshin@crplab.ru>
2015-03-16 17:02:37 +0300

    add quick chef installation script

---
+++ chef-omnibus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p /usr/local/portage/app-admin/chef-omnibus
+echo "PORTDIR_OVERLAY=/usr/local/portage" >> /etc/portage/make.conf
+wget https://raw.githubusercontent.com/laboshinl/gentoo-overlay/master/app-admin/chef-omnibus/chef-omnibus-12.1.1.ebuild -O /usr/local/portage/app-admin/chef-omnibus/chef-omnibus-12.1.1.ebuild
+wget https://raw.githubusercontent.com/laboshinl/gentoo-overlay/master/app-admin/chef-omnibus/Manifest -O /usr/local/portage/app-admin/chef-omnibus/Manifest
+ACCEPT_KEYWORDS='~amd64' emerge -u chef-omnibus
+emerge iproute2
+ln -s /bin/ip /sbin/ip
