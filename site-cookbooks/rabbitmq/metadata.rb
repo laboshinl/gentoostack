62c99f4652650ce23f332752da19fdb5158ff2e7
Leonid Laboshin <leonid.laboshin@crplab.ru>
2015-03-16 17:48:33 +0300

    initial rabbitmq

---
+++ site-cookbooks/rabbitmq/metadata.rb
@@ -0,0 +1,10 @@
+name             'rabbitmq'
+maintainer       'Leonid Laboshin'
+maintainer_email 'laboshinl@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures rabbitmq'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+supports         "gentoo"
+depends          "gentoo"
